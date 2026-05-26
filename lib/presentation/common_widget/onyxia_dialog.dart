58bba3e153e06bc6c6441ebe86e4f4abe9b9dc85
Roman Zinoviev <raizinoviev@gmail.com>
2026-05-26 02:30:21 -0400

    fixing onyxiadialog fringe

--- lib/presentation/common_widget/onyxia_dialog.dart
+++ lib/presentation/common_widget/onyxia_dialog.dart
@@ -23,56 +23,63 @@ class _OnyxiaDialogState extends ConsumerState<OnyxiaDialog> {
   Widget build(BuildContext context) {
     return Dialog(
       elevation: 4,
-      // TODO: there is a white fringe along the outer pixel edge of the dialog.
-      // TODO: cont. Clip.antiAliasWithSaveLayer has helped with it a bit but it's still noticeable and ugly.
-      clipBehavior: .antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(borderRadius: .circular(20)),
-      child: Container(
-        width: widget.width,
-        height: widget.height,
-        constraints: const BoxConstraints(minWidth: 300),
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      shadowColor: ThemeHelper.neutral100(context),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Material(
         color: ThemeHelper.neutral100(context),
-        child: Column(
-          spacing: 20,
-          children: [
-            // Header
-            Container(
-              padding: .fromLTRB(20, 20, 20, 0),
-              decoration: BoxDecoration(
-                color: ThemeHelper.neutral100(context),
-                borderRadius: .only(
-                  topLeft: .circular(20),
-                  topRight: .circular(20),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: .spaceBetween,
-                children: [
-                  Text(
-                    widget.title,
-                    style: NarwhalTextStyle(
-                      fontSize: 20,
-                      fontWeight: .w400,
-                      color: ThemeHelper.neutral800(context),
-                    ),
+        borderRadius: BorderRadius.circular(20),
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          width: widget.width,
+          height: widget.height,
+          constraints: const BoxConstraints(minWidth: 300),
+          color: ThemeHelper.neutral100(context),
+          child: Column(
+            spacing: 20,
+            children: [
+              // Header
+              Container(
+                padding: .fromLTRB(20, 20, 20, 0),
+                decoration: BoxDecoration(
+                  color: ThemeHelper.neutral100(context),
+                  borderRadius: .only(
+                    topLeft: .circular(20),
+                    topRight: .circular(20),
                   ),
-                ],
-              ),
-            ),
-
-            // Content
-            Expanded(
-              child: Padding(
-                padding: .fromLTRB(20, 0, 20, 20),
-                child: Column(
-                  crossAxisAlignment: .start,
+                ),
+                child: Row(
+                  mainAxisAlignment: .spaceBetween,
                   children: [
-                    widget.content != null ? widget.content! : const SizedBox(),
+                    Text(
+                      widget.title,
+                      style: NarwhalTextStyle(
+                        fontSize: 20,
+                        fontWeight: .w400,
+                        color: ThemeHelper.neutral800(context),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+
+              // Content
+              Expanded(
+                child: Padding(
+                  padding: .fromLTRB(20, 0, 20, 20),
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      widget.content != null
+                          ? widget.content!
+                          : const SizedBox(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
