b6757585b46057b6c1427f8656d05a4d5fe8bb48
Roman Zinoviev <raizinoviev@gmail.com>
2026-05-29 01:06:26 -0400

    comment

--- lib/presentation/shell/artifacts_sidebar/artifacts_sidebar_footer.dart
+++ lib/presentation/shell/artifacts_sidebar/artifacts_sidebar_footer.dart
@@ -19,6 +19,7 @@ class ArtifactsSidebarFooter extends ConsumerWidget {
       padding: .symmetric(horizontal: 12, vertical: 4),
       child: Row(
         children: [
+          // TODO: this onyxiabutton should open a menu above it (anchored topleft/bottomleft) that displays "manage vaults" at the bottom and above it other vaults that the user can access, readily available. will need to only show the three most recently-modified vaults to prevent overflow
           OnyxiaButton(label: vaultName, onTap: () => context.go(Routes.home)),
           const Spacer(),
           if (selectedVault != null) const VaultSettingsButton(),
