0a2ca50de6bade436a9f21b177df0bd4d702f72c
Roman Zinoviev <raizinoviev@gmail.com>
2026-06-01 00:38:07 -0400

    emailvalidationservice

---
+++ lib/services/email_validation_service.dart
@@ -0,0 +1,9 @@
+class EmailValidationService {
+  static bool isValidEmail(String email) {
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$',
+    );
+    return emailRegex.hasMatch(email);
+  }
+}
+// TODO: use this to validate "add member" form input
